266762cfa869513c51551cf23c527413127fab12
Gregorio Maclen <gregorio.maclen@gmail.com>
2021-04-06 20:04:44 -0300

    .

--- p2ej3.pas
+++ p2ej3.pas
@@ -1,4 +1,5 @@
 program p2ej3; //creo que bien pero no se puede probar. compila. al parecer hay que hacerlo usando el procedimiento minmo, no se como seria
+//hay que hacer una especie de merge acumulador, para no recorrer el maestro 30 veces
 const
 valoralto = 9999;
 type
@@ -19,6 +20,36 @@ procedure leer (var detalle:binDetalle; var producto:regDetalle);
     else
       producto.codigo:=valoralto;
   end;
+{procedure minimo (var V:vectorDeDetalles; var min:regDetalle);
+  var
+    i,iMin,valorMin:integer; Vreg:vectorDeRegistros;
+  begin
+    valorMin:=9999;
+    for i:= 1 to cantDetalles do begin
+      leer(V[i],Vreg[i]);
+      if (Vreg[i].codigo <> valoralto) & (Vreg[i].codigo > valorMin) then begin
+        valorMin:=Vreg[i].codigo;
+        iMin:=i;
+      end;
+    end;
+    min:=V[iMin];
+    leer(V[iMin],Vreg[iMin]);
+  end;
+procedure mergeAcumulador (var maestro:bin; var V:vectorDeDetalles);
+  var
+    actual,min:regObjeto; ult:lista;
+  begin
+    L:=nil;
+    minimo(V,min);
+    while (min.nombre<>'ZZZ') do begin
+      actual.nombre:=min.nombre; actual.monto:=0;
+      while (min.nombre<>'ZZZ') and (min.nombre=actual.nombre) do begin
+        actual.stockDis:=actual.stockDis+min.cantVendida;
+        minimo(V,min);
+      end;
+      agregarAtras(L,ult,actual);
+    end;
+  end;}
 procedure actualizarMaestro (var maestro:bin; var V:vector);
   var
     prodM:regProducto; prodD:regDetalle; i,aux,total:integer;
