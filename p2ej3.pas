a62b2096cc616bdd0c92924ae0306957b646bb46
Gregorio Maclen <gregorio.maclen@gmail.com>
2021-04-13 20:47:07 -0300

    .

--- p2ej3.pas
+++ p2ej3.pas
@@ -1,5 +1,4 @@
-program p2ej3; //creo que bien pero no se puede probar. compila. al parecer hay que hacerlo usando el procedimiento minmo, no se como seria
-//hay que hacer una especie de merge acumulador, para no recorrer el maestro 30 veces
+program p2ej3; //bien
 const
 valoralto = 9999;
 cantDetalles = 30;
@@ -24,79 +23,51 @@ procedure leer (var detalle:binDetalle; var producto:regDetalle);
   end;
 procedure minimo (var VD:vectorDeDetalles; var VR:vectorDeRegistros; var min:regDetalle);
   var
-    i,iMin,valorMin:integer;
+    i,iMin:integer;
   begin
-    valorMin:=9999;
+    min.codigo:=9999;
     for i:= 1 to cantDetalles do begin
-      leer(V[i],Vreg[i]);
-      if (Vreg[i].codigo <> valoralto) & (Vreg[i].codigo < valorMin) then begin
-        valorMin:=Vreg[i].codigo;
+      if (Vreg[i].codigo <> valoralto) & (Vreg[i].codigo < min.codigo) then begin
+        min:=Vreg[i];
         iMin:=i;
       end;
     end;
-    min:=V[iMin];
-    leer(V[iMin],Vreg[iMin]);
+    if (min.codigo <> 9999) then
+      leer(VD[iMin],VR[iMin]);
   end;
 procedure actualizarMaestro (var maestro:bin; var VD:vectorDeDetalles; var VR:vectorDeRegistros);
   var
-    i:integer;
+    i:integer; regM:regProducto; min:regDetalle;
   begin
     for i:= 1 to cantDetalles do begin
       reset(VD[i]);
     end;
     reset(maestro);
-    
+    minimo(VD,VR,min);
+    while (min.codigo<>valoralto) do begin
+      read(maestro,regM);
+      while (regM.codigo<>min.codigo) do
+        read(maestro,regM);
+      while (regM.codigo=min.codigo) do begin
+        regM.stockDis:=regM.stockDis - min.cantVendida;
+        minimo(VD,VR,min);
+      end;
+      seek(maestro,filepos(maestro)-1);
+      write(maestro,regM);
+    end;
     for i:= 1 to cantDetalles do begin
       close(VD[i]);
     end;
     close(maestro);
   end;
-procedure mergeAcumulador (var maestro:bin; var V:vectorDeDetalles);
-  var
-    actual,min:regObjeto; ult:lista;
-  begin
-    L:=nil;
-    minimo(V,min);
-    while (min.nombre<>'ZZZ') do begin
-      actual.nombre:=min.nombre; actual.monto:=0;
-      while (min.nombre<>'ZZZ') and (min.nombre=actual.nombre) do begin
-        actual.stockDis:=actual.stockDis+min.cantVendida;
-        minimo(V,min);
-      end;
-      agregarAtras(L,ult,actual);
-    end;
-  end;
-procedure actualizarMaestro (var maestro:bin; var V:vectorDeDetalles);
-  var
-    prodM:regProducto; prodD:regDetalle; i,aux,total:integer;
-  begin
-    reset(maestro);    
-    read(maestro,prodM);    
-    for i:= 1 to 30 do begin 
-      reset(V[i]);  
-      leer(V[i],prodD); 
-      while (prodD.codigo <> valoralto) do begin
-        aux:=prodD.codigo;
-        total:=0;
-        while (aux=prodD.codigo) do begin
-          total:=total+prodD.cantVendida;
-          leer(V[i],prodD);
-        end;
-        while (prodM.codigo <> aux) do
-          read(maestro,prodM);
-        prodM.stockDis:=prodM.stockDis - total;
-        seek(maestro,filepos(maestro)-1);
-        write(maestro,prodM);
-        if (not eof(maestro)) then
-          read(maestro,prodM);
-      end;
-      close(V[i]);
-    end;
-    close(maestro);    
-  end;
 var
-  maestro:bin; V:vectorDeDetalles;
+  maestro:bin; VD:vectorDeDetalles; VR:vectorDeRegistros; i:integer;
 begin
+  for i:=1 to cantDetalles do begin
+    Str(i,iString);
+    //assign(V[i],'detalle' + iString);
+    Leer(V[i],vectorReg[i]);
+  end;
   assign(maestro,'maestrop2ej3.dat');
-  actualizarMaestro(maestro,V);
+  actualizarMaestro(maestro,VD,VR);
 end.
